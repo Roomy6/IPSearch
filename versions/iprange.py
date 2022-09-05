6f348a3a86db02ac481e278e0c28af4bbb2b1fea
Roomy6 <73183827+Roomy6@users.noreply.github.com>
2022-09-05 21:54:12 +0100

    Update iprange.py

--- versions/iprange.py
+++ versions/iprange.py
@@ -1,24 +1,12 @@
-ip = input ("Enter the IP: ")
-splitip = ip.split(".") # remove the '.' from ip
-
-print (splitip)
-
-dot = "."
-
-startNum = int(input("Start number (1 - 255): "))
-#split1 = int(input("Enter first max range: "))
-#split2 = int(input("Enter second max range: "))
-#split3 = int(input("Enter third max range: "))
-#split4 = int(input("Enter fourth max range: "))
+from random import seed
+from random import randint
 
 # I have no idea what i am doing lol
 # im just trying to learn here
 
-while True:
-    for a in range(startNum, 255 + 1):
-        #print(str(a) + dot + str(a) + dot + str(a) + dot + str(a))
-        print(splitip[0] + dot + splitip[1] + dot + splitip[2] + dot + str(a))
+def ip_gen():
+    num = randint(0, 256)
+    return num
 
-        if a == 255:
-            for b in range(startNum, 255 + 1):
-                print(splitip[0] + dot + splitip[1] + dot + str(b) + dot + splitip[3])
+while True:
+    print(f'IP: {ip_gen()}.{ip_gen()}.{ip_gen()}.{ip_gen()}')
